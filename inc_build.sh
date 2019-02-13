fcf4333913c164979e35470aba236ce0b3a4fec7
John Gruber <john.t.gruber@gmail.com>
2019-02-12 18:20:41 -0600

    made inc_build check for OS

--- inc_build.sh
+++ inc_build.sh
@@ -3,5 +3,9 @@ buildstring=$(cat lx.spec|grep Release:|cut -d ' ' -f2)
 buildint=$(echo $buildstring|sed 's/^0*//')
 nextbuildint="$((buildint + 1))"
 printf -v nextbuildstring "%04d" $nextbuildint
-sed -i "s/^Release: .*/Release: $nextbuildstring/" lx.spec
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sed -i "s/^Release: .*/Release: $nextbuildstring/" lx.spec
+else
+    sed -i '' "s/^Release: .*/Release: $nextbuildstring/" lx.spec
+fi
 echo "Creating build $nextbuildstring"
