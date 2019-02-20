4ecf85a627f6a3bdb15b3f086cf3588a3014b74b
John Gruber <john.t.gruber@gmail.com>
2019-02-20 12:33:37 -0600

    multi OS build fix

--- inc_build.sh
+++ inc_build.sh
@@ -3,9 +3,12 @@ buildstring=$(cat lx.spec|grep Release:|cut -d ' ' -f2)
 buildint=$(echo $buildstring|sed 's/^0*//')
 nextbuildint="$((buildint + 1))"
 printf -v nextbuildstring "%04d" $nextbuildint
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+IS_LINUX=$(uname -o | grep inux | wc -l)
+if [ $IS_LINUX == 1 ]; then
+    echo "running linux sed"
     sed -i "s/^Release: .*/Release: $nextbuildstring/" lx.spec
 else
+    echo "running non Linux sed"
     sed -i '' "s/^Release: .*/Release: $nextbuildstring/" lx.spec
 fi
 echo "Creating build $nextbuildstring"
